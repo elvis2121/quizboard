5455f4ee5566f7edb2fdb6ce09f70a8da8bba62f
elvis2121 <elvisnjau@gmail.com>
2019-03-29 08:55:18 +0300

    using jquery to get data from the  form

--- result.js
+++ result.js
@@ -2,3 +2,22 @@ var user_input =[];
 var answers = ['compiled language','12','prompt','vowel','Modular JavaScript'];
 var score = 0;
 var res1,res2,res3,res4,res5,marks,grade;
+
+
+$(document).ready(function() {
+  $(".results").hide();
+  $("#submit_button").click(function () {
+    answer1 = $("input[name='question1']:checked").val();
+    user_input.push(answer1);
+    
+    answer2 =$("input[name='question2']:checked").val();
+    user_input.push(answer2);
+    
+    answer3 =$("input[name='question3']:checked").val();
+    user_input.push(answer3);
+    
+    answer4 =$("input[name='question4']:checked").val();
+    user_input.push(answer4);
+    
+    answer5 =$("input[name='question5']:checked").val();
+    user_input.push(answer5);
