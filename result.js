f522e0cad27413a782248a39cdaaedf58a2f292e
elvis2121 <elvisnjau@gmail.com>
2019-03-29 09:24:43 +0300

    indenting and commenting on html file

--- result.js
+++ result.js
@@ -42,6 +42,28 @@ $(document).ready(function() {
 
 });
 
+$(document).ready(function(){
+  $("#submit_button").click(function() {
+    $(".results").show();    
+    $(".results").html("YOU HAVE SCORED: ".concat(marks) + "<br> WHICH IS GRADE: ".concat(grade));
+    $(".results").animate({top: '850px',left:'400px'},"slow");
+    
+    function blink_text() {
+      $('.results').fadeToggle(1500);
+    }
+    setInterval(blink_text, 1000);    
+    
+  });
+});
+
+
+
+
+$('#reset_button').click(function () {
+		$('input[type="radio"]').prop('checked', false);
+    location.reload();
+	
+});
     
     
     
