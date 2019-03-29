f139fd9411680aa178598429e74e056e2415a622
elvis2121 <elvisnjau@gmail.com>
2019-03-29 09:35:42 +0300

    indenting and commenting on the js file

--- result.js
+++ result.js
@@ -1,3 +1,4 @@
+
 var user_input =[];
 var answers = ['compiled language','12','prompt','vowel','Modular JavaScript'];
 var score = 0;
@@ -6,8 +7,7 @@ var res1,res2,res3,res4,res5,marks,grade;
 function addition(val1,val2,val3,val4,val5){
   return(val1+val2+val3+val4+val5);
 }
-
-
+// Business logic
 
 $(document).ready(function() {
   $(".results").hide();
@@ -25,22 +25,49 @@ $(document).ready(function() {
     user_input.push(answer4);
     
     answer5 =$("input[name='question5']:checked").val();
-    user_input.push(answer5);
+    user_input.push(answer5);    
+    
+    if(user_input[0] === answers[0]){
+      res1=20;
+    }else {
+      res1=0;
+    }
+    if (user_input[1]===answers[1]) {
+      res2=20;      
+    }else {
+      res2=0;
+    }
+    if (user_input[2]=== answers[2]) {
+      res3=20;      
+    }else {
+      res3=0;
+    }
+    if (user_input[3] === answers[3]) {
+      res4=20;
+      
+    }else {
+      res4 = 0;
+    }
+    if (user_input[4]=== answers[4]) {
+      res5=20;
+    }else {
+      res5=0;
+    }
     
     marks =addition(res1,res2,res3,res4,res5);
     
     if (marks>=80) {
-    grade= "EXCELLENT";      
-  }if (marks>=50 && marks<80) {
-    grade= "FAIR";      
-  }if (marks<50) {
-    grade="POOR";      
-  } 
-  
-  
-});  
-
+      grade= "EXCELLENT";      
+    }if (marks>=50 && marks<80) {
+      grade= "FAIR";      
+    }if (marks<50) {
+      grade="POOR";      
+    }     
+  }); 
 });
+
+
+// interface logic
 
 $(document).ready(function(){
   $("#submit_button").click(function() {
@@ -52,44 +79,10 @@ $(document).ready(function(){
       $('.results').fadeToggle(1500);
     }
     setInterval(blink_text, 1000);    
-    
   });
 });
 
-
-
-
 $('#reset_button').click(function () {
 		$('input[type="radio"]').prop('checked', false);
-    location.reload();
-	
+    location.reload();	
 });
-    
-    
-    
-  if(user_input[0] === answers[0]){
-    res1=20;
-  }else {
-    res1=0;
-  }
-  if (user_input[1]===answers[1]) {
-    res2=20;      
-  }else {
-    res2=0;
-  }
-  if (user_input[2]=== answers[2]) {
-    res3=20;      
-  }else {
-    res3=0;
-  }
-  if (user_input[3] === answers[3]) {
-    res4=20;
-    
-  }else {
-    res4 = 0;
-  }
-  if (user_input[4]=== answers[4]) {
-    res5=20;
-  }else {
-    res5=0;
-  }
