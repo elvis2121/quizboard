46087466882e4d132dad3aa9bee1a52a84337a09
elvis2121 <elvisnjau@gmail.com>
2019-03-29 08:56:37 +0300

    using conditional statements to assess values from the form

--- result.js
+++ result.js
@@ -21,3 +21,32 @@ $(document).ready(function() {
     
     answer5 =$("input[name='question5']:checked").val();
     user_input.push(answer5);
+    
+    
+    
+  if(user_input[0] === answers[0]){
+    res1=20;
+  }else {
+    res1=0;
+  }
+  if (user_input[1]===answers[1]) {
+    res2=20;      
+  }else {
+    res2=0;
+  }
+  if (user_input[2]=== answers[2]) {
+    res3=20;      
+  }else {
+    res3=0;
+  }
+  if (user_input[3] === answers[3]) {
+    res4=20;
+    
+  }else {
+    res4 = 0;
+  }
+  if (user_input[4]=== answers[4]) {
+    res5=20;
+  }else {
+    res5=0;
+  }
