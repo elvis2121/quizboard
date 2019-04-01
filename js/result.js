f25b66aea39494d22d6c34e0d6dfb071184b5456
elvis2121 <elvisnjau@gmail.com>
2019-04-01 09:44:05 +0300

    added bootstrap classes to buttons and styled the main header

--- js/result.js
+++ js/result.js
@@ -1,7 +1,7 @@
 
 var user_Input =[];
 var answers = ['compiled language','12','prompt','vowel','Modular JavaScript'];
-var res1,res2,res3,res4,res5,marks,grade;
+var result1,result2,result3,result4,result5,marks,grade;
 
 function addition(val1,val2,val3,val4,val5){
   return(val1+val2+val3+val4+val5);
@@ -9,33 +9,33 @@ function addition(val1,val2,val3,val4,val5){
 // Business logic    
     
     if(user_Input[0] === answers[0]){
-      res1=20;
+      result1=20;
     }else {
-      res1=0;
+      result1=0;
     }
     if (user_Input[1]===answers[1]) {
-      res2=20;      
+      result2=20;      
     }else {
-      res2=0;
+      result2=0;
     }
     if (user_Input[2]=== answers[2]) {
-      res3=20;      
+      result3=20;      
     }else {
-      res3=0;
+      result3=0;
     }
     if (user_Input[3] === answers[3]) {
-      res4=20;
+      result4=20;
       
     }else {
-      res4 = 0;
+      result4 = 0;
     }
     if (user_Input[4]=== answers[4]) {
-      res5=20;
+      result5=20;
     }else {
-      res5=0;
+      result5=0;
     }
     
-    marks =addition(res1,res2,res3,res4,res5);
+    marks =addition(result1,result2,result3,result4,result5);
     
     if (marks>=80) {
       grade= "EXCELLENT";      
@@ -73,7 +73,8 @@ $(document).ready(function() {
 $(document).ready(function(){
   $("#submit_button").click(function() {
     $(".results").show();    
-    $(".results").html("YOU HAVE SCORED: ".concat(marks) + "<br> WHICH IS GRADE: ".concat(grade));
+    $(".results").html("YOU HAVE SCORED: ".concat(marks) + "<br> WHICH IS GRADE:
+     ".concat(grade));
     $(".results").animate({top: '850px',left:'400px'},"slow");
     
     function blink_text() {
